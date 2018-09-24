5397d6d4c63db4d1df7a28a3d1daefdc99d0d38d
Axel Rindle <axel.rindle@gmx.de>
2018-09-24 21:53:19 +0200

    Add example for using the rest api

---
+++ examples/rest.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+'use strict';
+
+// register coffeescript
+require('coffeescript/register');
+
+const versionCheck = require('../lib/main');
+const options = {
+  token: '',
+  repo: 'github-version-checker',
+  owner: 'axelrindle',
+  currentVersion: require('../package.json').version
+};
+
+versionCheck(options)
+  .then(function (update) {
+    if (update) { // update is null if there is no update available, so check here
+      console.log("An update is available! " + update.name);
+      console.log("You are on version " + options.currentVersion + "!");
+    } else {
+      console.log("You are up to date.");
+    }
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
