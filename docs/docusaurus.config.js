6e49669ee77277f6ae5676a818f960ca94ddabc8
Axel Rindle <mail@axelrindle.de>
2022-12-20 14:57:30 +0100

    Fix type

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
   ],
 
   themeConfig:
-    /** @type {import('docusaurus-preset-nonepress').ThemeConfig} */
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
         title: 'github-version-checker',
